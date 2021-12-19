65126d4ad88b7f52fd3c2110d55753f813fcdde1
Hyeonjong <nianelo4@gmail.com>
2021-12-19 13:52:36 +0900

    Install babel with some configuration

--- webpack.config.js
+++ webpack.config.js
@@ -10,4 +10,20 @@ module.exports = {
       type: 'umd',
     },
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+            ],
+          },
+        },
+      },
+    ],
+  },
 };
