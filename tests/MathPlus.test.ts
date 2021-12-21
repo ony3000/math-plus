e0d5010ca21bd43a81c2a15016d21d3d12280fb4
Hyeonjong <nianelo4@gmail.com>
2021-12-21 23:00:12 +0900

    Describe test suite in more detail

--- tests/MathPlus.test.ts
+++ tests/MathPlus.test.ts
@@ -1,15 +1,17 @@
 import MathPlus from '../src';
 
-describe('Check the state of both objects.', () => {
-  test('isExtensible', () => {
-    expect(Object.isExtensible(MathPlus)).toBe(Object.isExtensible(Math));
-  });
+describe('The same functionality as the built-in object `Math` should be guaranteed.', () => {
+  describe('Both objects must have the same characteristics.', () => {
+    test('isExtensible', () => {
+      expect(Object.isExtensible(MathPlus)).toBe(Object.isExtensible(Math));
+    });
 
-  test('isSealed', () => {
-    expect(Object.isSealed(MathPlus)).toBe(Object.isSealed(Math));
-  });
+    test('isSealed', () => {
+      expect(Object.isSealed(MathPlus)).toBe(Object.isSealed(Math));
+    });
 
-  test('isFrozen', () => {
-    expect(Object.isFrozen(MathPlus)).toBe(Object.isFrozen(Math));
+    test('isFrozen', () => {
+      expect(Object.isFrozen(MathPlus)).toBe(Object.isFrozen(Math));
+    });
   });
 });
