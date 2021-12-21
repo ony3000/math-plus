1de67eb503fbe2bb5c709fb2c661fa70b81b6df1
Hyeonjong <nianelo4@gmail.com>
2021-12-21 23:19:55 +0900

    Update eslint configuration by adding typescript related settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,17 +7,19 @@ module.exports = {
   },
   extends: [
     'airbnb-base',
+    'airbnb-typescript/base',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 13,
     sourceType: 'module',
+    project: './tsconfig.json',
   },
   plugins: [
     '@typescript-eslint',
   ],
   rules: {
-    'brace-style': [
+    '@typescript-eslint/brace-style': [
       'error',
       'stroustrup',
     ],
