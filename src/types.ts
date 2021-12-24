ec32dee3c50fceb32d2feb0df5e455c094c426cb
Hyeonjong <nianelo4@gmail.com>
2021-12-25 00:14:21 +0900

    Write aliased type across multiple lines

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,13 @@
-type MathConstant = 'E' | 'LN10' | 'LN2' | 'LOG10E' | 'LOG2E' | 'PI' | 'SQRT1_2' | 'SQRT2';
+type MathConstant =
+  'E'
+  | 'LN10'
+  | 'LN2'
+  | 'LOG10E'
+  | 'LOG2E'
+  | 'PI'
+  | 'SQRT1_2'
+  | 'SQRT2'
+  ;
 
 export {
   MathConstant,
