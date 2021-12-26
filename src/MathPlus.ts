7e8c10095558017206e7e8d1d457c8c0532b412a
Hyeonjong <nianelo4@gmail.com>
2021-12-26 21:26:17 +0900

    Add 'abs' method to 'MathPlus'

--- src/MathPlus.ts
+++ src/MathPlus.ts
@@ -10,6 +10,9 @@ const MathPlus: MathPlusShape = {
   PI: Math.PI,
   SQRT1_2: Math.SQRT1_2,
   SQRT2: Math.SQRT2,
+  abs(x: number): number {
+    return Math.abs(x);
+  },
 };
 
 export const constantProps = Object.getOwnPropertyNames(Math).filter((prop) => {
@@ -32,6 +35,14 @@ export const methodProps = Object.getOwnPropertyNames(Math).filter((prop) => {
   const descriptor = Object.getOwnPropertyDescriptor(Math, prop);
 
   return (descriptor !== undefined && typeof descriptor.value === 'function');
+});
+
+const methodDescriptor: PropertyDescriptor = {
+  enumerable: false,
+};
+
+methodProps.forEach((prop) => {
+  Object.defineProperty(MathPlus, prop, methodDescriptor);
 });
 
 export default MathPlus;
