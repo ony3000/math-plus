cc1cd6d5b81919aa9488fff59aae06ba2b2d1758
Hyeonjong <nianelo4@gmail.com>
2022-01-01 23:00:10 +0900

    Remove tests that compare result of method call to actual value

--- tests/MathPlus.test.js
+++ tests/MathPlus.test.js
@@ -80,36 +80,19 @@ describe('The same functionality as the built-in object `Math` should be guarant
       const extendedFunction = MathPlus.abs;
       const isDefinedAsFunction = typeof extendedFunction === 'function';
 
-      testIf(isDefinedAsFunction, 'passing a numeric value returns the absolute value', () => {
+      testIf(isDefinedAsFunction, 'passing a numeric value (except bigint)', () => {
         expect(extendedFunction('-1')).toBe(originalFunction('-1'));
         expect(extendedFunction(-2)).toBe(originalFunction(-2));
-
-        expect(extendedFunction('-1')).toBe(1);
-        expect(extendedFunction(-2)).toBe(2);
       });
 
-      testIf(isDefinedAsFunction, 'passing an empty object, an array with more than one member, a non-numeric string, undefined or empty parameter returns NaN', () => {
+      testIf(isDefinedAsFunction, 'passing a non-numeric value', () => {
+        expect(extendedFunction(null)).toBe(originalFunction(null));
+        expect(extendedFunction('')).toBe(originalFunction(''));
+        expect(extendedFunction([])).toBe(originalFunction([]));
         expect(extendedFunction({})).toBe(originalFunction({}));
         expect(extendedFunction([1, 2])).toBe(originalFunction([1, 2]));
         expect(extendedFunction('string')).toBe(originalFunction('string'));
         expect(extendedFunction(undefined)).toBe(originalFunction(undefined));
-        expect(extendedFunction()).toBe(originalFunction());
-
-        expect(extendedFunction({})).toBe(NaN);
-        expect(extendedFunction([1, 2])).toBe(NaN);
-        expect(extendedFunction('string')).toBe(NaN);
-        expect(extendedFunction(undefined)).toBe(NaN);
-        expect(extendedFunction()).toBe(NaN);
-      });
-
-      testIf(isDefinedAsFunction, 'passing null, an empty string or an empty array returns 0', () => {
-        expect(extendedFunction(null)).toBe(originalFunction(null));
-        expect(extendedFunction('')).toBe(originalFunction(''));
-        expect(extendedFunction([])).toBe(originalFunction([]));
-
-        expect(extendedFunction(null)).toBe(0);
-        expect(extendedFunction('')).toBe(0);
-        expect(extendedFunction([])).toBe(0);
       });
 
       testIf(isDefinedAsFunction, 'passing a bigint throws error', () => {
@@ -117,47 +100,25 @@ describe('The same functionality as the built-in object `Math` should be guarant
         expect(() => extendedFunction(1n)).toThrow();
       });
 
-      describeIf(isDefinedAsFunction, 'passing an array with only one member is almost the same as passing that member as is, but...', () => {
-        test('a numeric value returns the absolute value', () => {
+      describeIf(isDefinedAsFunction, 'passing an array with only one member is almost the same as passing that member as is', () => {
+        test('an array with only one numeric value (except bigint)', () => {
           expect(extendedFunction(['-1'])).toBe(originalFunction(['-1']));
           expect(extendedFunction([-2])).toBe(originalFunction([-2]));
-
-          expect(extendedFunction(['-1'])).toBe(1);
-          expect(extendedFunction([-2])).toBe(2);
         });
 
-        test('an empty object, an array with more than one member or a non-numeric string returns NaN', () => {
-          expect(extendedFunction([{}])).toBe(originalFunction([{}]));
-          expect(extendedFunction([[1, 2]])).toBe(originalFunction([[1, 2]]));
-          expect(extendedFunction(['string'])).toBe(originalFunction(['string']));
-
-          expect(extendedFunction([{}])).toBe(NaN);
-          expect(extendedFunction([[1, 2]])).toBe(NaN);
-          expect(extendedFunction(['string'])).toBe(NaN);
-        });
-
-        test('[!] undefined not return NaN, and returns 0', () => {
-          expect(extendedFunction([undefined])).toBe(originalFunction([undefined]));
-
-          expect(extendedFunction([undefined])).toBe(0);
-        });
-
-        test('null, an empty string or an empty array returns 0', () => {
+        test('an array with only one non-numeric value', () => {
           expect(extendedFunction([null])).toBe(originalFunction([null]));
           expect(extendedFunction([''])).toBe(originalFunction(['']));
           expect(extendedFunction([[]])).toBe(originalFunction([[]]));
-
-          expect(extendedFunction([null])).toBe(0);
-          expect(extendedFunction([''])).toBe(0);
-          expect(extendedFunction([[]])).toBe(0);
+          expect(extendedFunction([{}])).toBe(originalFunction([{}]));
+          expect(extendedFunction([[1, 2]])).toBe(originalFunction([[1, 2]]));
+          expect(extendedFunction(['string'])).toBe(originalFunction(['string']));
+          expect(extendedFunction([undefined])).toBe(originalFunction([undefined]));
         });
 
-        test('[!] a bigint not throw error, and returns the absolute value', () => {
+        test('[!] an array with only one bigint not throw error', () => {
           expect(extendedFunction([-2n])).toBe(originalFunction([-2n]));
           expect(extendedFunction([1n])).toBe(originalFunction([1n]));
-
-          expect(extendedFunction([-2n])).toBe(2);
-          expect(extendedFunction([1n])).toBe(1);
         });
       });
     });
@@ -167,44 +128,23 @@ describe('The same functionality as the built-in object `Math` should be guarant
       const extendedFunction = MathPlus.acos;
       const isDefinedAsFunction = typeof extendedFunction === 'function';
 
-      testIf(isDefinedAsFunction, 'passing a number between -1 and 1 returns the arc-cosine of the given number', () => {
+      testIf(isDefinedAsFunction, 'passing a number', () => {
+        expect(extendedFunction(-2)).toBe(originalFunction(-2));
         expect(extendedFunction(-1)).toBe(originalFunction(-1));
         expect(extendedFunction(0)).toBe(originalFunction(0));
+        expect(extendedFunction(0.5)).toBe(originalFunction(0.5));
         expect(extendedFunction(1)).toBe(originalFunction(1));
-
-        expect(extendedFunction(-1)).toBe(Math.PI);
-        expect(extendedFunction(0)).toBe(Math.PI / 2);
-        expect(extendedFunction(1)).toBe(0);
-      });
-
-      testIf(isDefinedAsFunction, 'passing a number less than -1 or greater than 1 returns NaN', () => {
-        expect(extendedFunction(-2)).toBe(originalFunction(-2));
         expect(extendedFunction(2)).toBe(originalFunction(2));
-
-        expect(extendedFunction(-2)).toBe(NaN);
-        expect(extendedFunction(2)).toBe(NaN);
       });
 
-      testIf(isDefinedAsFunction, 'passing null, an empty string or an empty array returns the arc-cosine of 0', () => {
+      testIf(isDefinedAsFunction, 'passing a non-numeric value', () => {
         expect(extendedFunction(null)).toBe(originalFunction(null));
         expect(extendedFunction('')).toBe(originalFunction(''));
         expect(extendedFunction([])).toBe(originalFunction([]));
-
-        expect(extendedFunction(null)).toBe(Math.PI / 2);
-        expect(extendedFunction('')).toBe(Math.PI / 2);
-        expect(extendedFunction([])).toBe(Math.PI / 2);
-      });
-
-      testIf(isDefinedAsFunction, 'passing a non-numeric value (except the three above) returns NaN', () => {
         expect(extendedFunction({})).toBe(originalFunction({}));
         expect(extendedFunction([1, 2])).toBe(originalFunction([1, 2]));
         expect(extendedFunction('string')).toBe(originalFunction('string'));
-        expect(extendedFunction()).toBe(originalFunction());
-
-        expect(extendedFunction({})).toBe(NaN);
-        expect(extendedFunction([1, 2])).toBe(NaN);
-        expect(extendedFunction('string')).toBe(NaN);
-        expect(extendedFunction()).toBe(NaN);
+        expect(extendedFunction(undefined)).toBe(originalFunction(undefined));
       });
 
       testIf(isDefinedAsFunction, 'passing a bigint throws error', () => {
@@ -219,40 +159,22 @@ describe('The same functionality as the built-in object `Math` should be guarant
       const extendedFunction = MathPlus.acosh;
       const isDefinedAsFunction = typeof extendedFunction === 'function';
 
-      testIf(isDefinedAsFunction, 'passing a number greater than or equal to 1 returns the hyperbolic arc-cosine of the given number', () => {
-        expect(extendedFunction(1)).toBe(originalFunction(1));
-        expect(extendedFunction(2)).toBe(originalFunction(2));
-
-        expect(extendedFunction(1)).toBe(0);
-        expect(extendedFunction(2)).toBe(Math.log(2 + Math.sqrt(2 ** 2 - 1)));
-      });
-
-      testIf(isDefinedAsFunction, 'passing a number less than 1 returns NaN', () => {
+      testIf(isDefinedAsFunction, 'passing a number', () => {
         expect(extendedFunction(-1)).toBe(originalFunction(-1));
         expect(extendedFunction(0)).toBe(originalFunction(0));
         expect(extendedFunction(0.5)).toBe(originalFunction(0.5));
-
-        expect(extendedFunction(-1)).toBe(NaN);
-        expect(extendedFunction(0)).toBe(NaN);
-        expect(extendedFunction(0.5)).toBe(NaN);
+        expect(extendedFunction(1)).toBe(originalFunction(1));
+        expect(extendedFunction(2)).toBe(originalFunction(2));
       });
 
-      testIf(isDefinedAsFunction, 'passing a non-numeric value returns NaN', () => {
+      testIf(isDefinedAsFunction, 'passing a non-numeric value', () => {
         expect(extendedFunction(null)).toBe(originalFunction(null));
         expect(extendedFunction('')).toBe(originalFunction(''));
         expect(extendedFunction([])).toBe(originalFunction([]));
         expect(extendedFunction({})).toBe(originalFunction({}));
         expect(extendedFunction([1, 2])).toBe(originalFunction([1, 2]));
         expect(extendedFunction('string')).toBe(originalFunction('string'));
-        expect(extendedFunction()).toBe(originalFunction());
-
-        expect(extendedFunction(null)).toBe(NaN);
-        expect(extendedFunction('')).toBe(NaN);
-        expect(extendedFunction([])).toBe(NaN);
-        expect(extendedFunction({})).toBe(NaN);
-        expect(extendedFunction([1, 2])).toBe(NaN);
-        expect(extendedFunction('string')).toBe(NaN);
-        expect(extendedFunction()).toBe(NaN);
+        expect(extendedFunction(undefined)).toBe(originalFunction(undefined));
       });
 
       testIf(isDefinedAsFunction, 'passing a bigint throws error', () => {
