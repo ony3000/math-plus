b11721c8507d7cdb11c9e1969dd30d20215e5763
Hyeonjong <nianelo4@gmail.com>
2022-01-02 19:11:50 +0900

    Add remaining methods that start with 'a' to 'MathPlus'

--- src/MathPlus.ts
+++ src/MathPlus.ts
@@ -19,6 +19,21 @@ const MathPlus: MathPlusShape = {
   acosh(x: number): number {
     return Math.acosh(x);
   },
+  asin(x: number): number {
+    return Math.asin(x);
+  },
+  asinh(x: number): number {
+    return Math.asinh(x);
+  },
+  atan(x: number): number {
+    return Math.atan(x);
+  },
+  atan2(y: number, x: number): number {
+    return Math.atan2(y, x);
+  },
+  atanh(x: number): number {
+    return Math.atanh(x);
+  },
 };
 
 export const constantProps = Object.getOwnPropertyNames(Math).filter((prop) => {
